d14fbcb3c9f50f9e2c9906779ee9485966a547fc
kuc00000 <kwchany@naver.com>
2023-09-12 16:49:27 +0900

    adadad

--- fintech/src/components/StyledComponent.js
+++ fintech/src/components/StyledComponent.js
@@ -6,6 +6,8 @@ const BodyBlock = styled.div`
    color: red;
    -webkit-box-shadow: 13px 6px 15px 5px #000000; 
   box-shadow: 13px 6px 15px 5px #000000;
+  padding: 20px;
+  maring: 10px;
    `;
 const StyledComponent = () => {
    
